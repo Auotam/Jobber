fd9d0946c39cb1ef6616ce53baec4009284dd401
Shivam Sharma <shivams905@gmail.com>
2024-02-16 20:52:47 +0530

    new header

--- layout/headers/header.jsx
+++ layout/headers/header.jsx
@@ -41,7 +41,7 @@ const HeaderOne = () => {
                                     <div className="header-main-left header-main-left-header1">
                                         <div className="header-logo header1-logo">
                                             <Link href="/"><a className="logo-bl"><img src="/assets/img/logo/logo-w-nbg.svg"
-                                                alt="logo-img" /></a></Link>
+                                                alt="logo-imgnew" /></a></Link>
                                         </div>
                                         <div className="main-menu main-menu1 d-none d-xl-block">
                                             <nav id="mobile-menu">
