1db51dc74a3011196b1bc22e9929919fc3550ad5
Shivam Sharma <shivams905@gmail.com>
2024-04-04 23:25:23 +0530

    404 page new

--- pages/404.js
+++ pages/404.js
@@ -22,10 +22,10 @@ const index = () => {
 		
 		<div className="contant_box_404">
 		<h3 className="h2">
-		Look like you're lost
+		Page not found
 		</h3>
 		
-		<p>the page you are looking for not avaible!</p>
+		<p>The page you are looking for not avaible!</p>
 		
 		<a href="/" className="link_404">Go to Home</a>
 	</div>
